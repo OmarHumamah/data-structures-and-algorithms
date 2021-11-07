649062794829b93c05eca2647fa6f8c3720257dc
OmarHumamah <omar.nabeel.h@gmail.com>
2021-11-07 18:33:37 +0200

    code05

---
+++ code-challenge-401/code05/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const LinkedList = require("./linkedlist");
+
+const ll = new LinkedList();
+
+ll.insert("1");
+ll.insert("2");
+ll.insert("3");
+ll.insert("a");
+ll.insert("b");
+ll.insert("c");
+
+console.log(ll);
